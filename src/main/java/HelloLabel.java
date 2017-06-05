b436b6792d99c2f3f7e43c9812a9a964adf555c0
lizhaofu <lizhaofu0215@163.com>
2017-06-05 20:35:01 +0800

    ss

--- src/main/java/HelloLabel.java
+++ src/main/java/HelloLabel.java
@@ -8,6 +8,7 @@ import java.util.concurrent.TimeUnit;
 public class HelloLabel {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Hello Swing");
+        int i = 0;
         JLabel label = new JLabel("A Label");
         frame.add(label);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
